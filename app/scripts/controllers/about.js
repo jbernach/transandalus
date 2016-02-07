0100fc78ec0a9f0d90e753f5c87dec1271d8a62e
linuxonrails <linuxonrails@linuxonrails.com>
2016-02-07 21:01:56 +0100

    Instalación de Angular y Bootstrap con un generator de Yeoman

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name transandalusApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the transandalusApp
+ */
+angular.module('transandalusApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
