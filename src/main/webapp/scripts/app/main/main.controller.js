8f45fb5514817b702f2563b7fa6cd7ee04fc127b
José María Berná Chamber <jbernach@gmail.com>
2016-06-14 23:30:27 +0200

    Index redesign.

--- src/main/webapp/scripts/app/main/main.controller.js
+++ src/main/webapp/scripts/app/main/main.controller.js
@@ -1,9 +1,18 @@
 'use strict';
 
 angular.module('transandalus')
-    .controller('MainController', function ($scope, Principal) {
+    .controller('MainController', function ($scope, Principal, Province) {
         Principal.identity().then(function(account) {
             $scope.account = account;
             $scope.isAuthenticated = Principal.isAuthenticated;
         });
+
+        $scope.provinces = [];
+        $scope.loadAll = function() {
+            Province.query({page: 0, size: 50, sort: ['id']}, function(result, headers) {
+                $scope.provinces = result;
+            });
+        };
+        
+        $scope.loadAll();
     });
