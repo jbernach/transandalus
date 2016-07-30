175739cb1d1511289709c469292c4d794d5206e9
José María Berná Chamber <jbernach@gmail.com>
2016-07-30 15:38:33 +0200

    First test rendering.

--- src/main/webapp/scripts/app/vita/vita.controller.js
+++ src/main/webapp/scripts/app/vita/vita.controller.js
@@ -1,9 +1,32 @@
 'use strict';
 
 angular.module('transandalus')
-    .controller('VitaController', function ($scope, Principal) {
+    .controller('VitaController', function ($scope, Principal, Stage, API_URL) {
        Principal.identity().then(function(account) {
             $scope.account = account;
             $scope.isAuthenticated = Principal.isAuthenticated;
         });
+
+       $scope.map = {
+            center: { //Cordoba
+              latitude: 37.891581,
+              longitude: -4.778564
+            },
+            zoom:7,
+            options: {scrollwheel:false}
+        };
+
+        $scope.map.kmlLayerOptions = {'url':''};
+
+        $scope.loadAllStages = function() {
+            Stage.query({page: 0, size: 18, sort: ['id']}, function(result, headers) {
+               for (var i = 0; i < result.length; i++) {
+                   result[i].kmlLayerOptions = {url: API_URL + '/tracks/'+result[i].trackId};
+               }
+             
+                $scope.stages = result;
+            });
+        };
+       
+        $scope.loadAllStages();
     });
