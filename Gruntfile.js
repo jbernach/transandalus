f380cf4a844cdcd19b1a2ba465f2c46b99b5c6c1
José María Berná Chamber <jbernach@gmail.com>
2017-03-13 22:22:03 +0100

    province

--- Gruntfile.js
+++ Gruntfile.js
@@ -254,8 +254,13 @@ module.exports = function (grunt) {
                 },{
                     expand: true,
                     cwd: 'src/main/webapp/bower_components/angular-i18n/',
-                    src: '*.js',
+                    src: '{angular-locale_es,angular-locale_en,es,en}.js',
                     dest: '<%= yeoman.dist %>/bower_components/angular-i18n'
+                },{
+                    expand: true,
+                    cwd: 'src/main/webapp/uploaded-images',
+                    src: '**/*.{png,gif,webp,jpg,jpeg,svg}',
+                    dest: '<%= yeoman.dist %>/uploaded-images'
                 }]
             }
         },
