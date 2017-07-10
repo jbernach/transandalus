ff9c0cef9605c0398ac36e3607e5447587d163c8
José María Berná Chamber <jbernach@gmail.com>
2017-07-10 14:19:47 +0200

    remove cacheBuster on vita

--- src/main/webapp/scripts/app/vita/vita.controller.js
+++ src/main/webapp/scripts/app/vita/vita.controller.js
@@ -74,15 +74,15 @@ angular.module('transandalus')
         $scope.updateLayers = function(){
             $scope.kmlLayers = {
                 track: {
-                    url: API_URL + '/layer/track?alt=' + $scope.showAlternatives + '&link=' + $scope.showLinks + '&cacheBuster=' + (new Date()).getTime(),
+                    url: API_URL + '/layer/track?alt=' + $scope.showAlternatives + '&link=' + $scope.showLinks,
                     preserveViewport: true
                 },
                 markers: {
-                    url: API_URL + '/layer/marcadores?alt=' + $scope.showAlternatives + '&link=' + $scope.showLinks + '&cacheBuster=' + (new Date()).getTime(),
+                    url: API_URL + '/layer/marcadores?alt=' + $scope.showAlternatives + '&link=' + $scope.showLinks,
                     preserveViewport: true
                 },
                 services: {
-                    url: API_URL + '/layer/servicios?alt=' + $scope.showAlternatives + '&link=' + $scope.showLinks + '&cacheBuster=' + (new Date()).getTime(),
+                    url: API_URL + '/layer/servicios?alt=' + $scope.showAlternatives + '&link=' + $scope.showLinks,
                     preserveViewport: true
                 }
             };
